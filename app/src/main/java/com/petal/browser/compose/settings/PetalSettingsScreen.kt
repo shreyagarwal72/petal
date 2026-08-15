16a603fd0fd8b638ed963b8fb4edf55aa59fe5e7
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-08-16 02:28:00 +0530

    Fix graphicsLayer import to androidx.compose.ui.graphics.graphicsLayer

--- app/src/main/java/com/petal/browser/compose/settings/PetalSettingsScreen.kt
+++ app/src/main/java/com/petal/browser/compose/settings/PetalSettingsScreen.kt
@@ -43,7 +43,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.graphicsLayer
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.ComposeView
