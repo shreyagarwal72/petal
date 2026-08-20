6440fa0dcc389b35036c634a35872c515e35b9c1
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-08-20 20:14:28 +0530

    fix(downloads): add missing background import in PetalDownloadManagerScreen

--- app/src/main/java/com/petal/browser/compose/downloads/PetalDownloadManagerScreen.kt
+++ app/src/main/java/com/petal/browser/compose/downloads/PetalDownloadManagerScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
