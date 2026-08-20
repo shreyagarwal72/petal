ae43af9c8b73c4be89c95866b015cd338fb00031
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-08-20 23:29:27 +0530

    fix(touch): prevent crash on tap by checking null MotionEvent in SwipeTouchListener

--- app/src/main/java/com/petal/browser/view/SwipeTouchListener.java
+++ app/src/main/java/com/petal/browser/view/SwipeTouchListener.java
@@ -52,36 +52,31 @@ public class SwipeTouchListener implements OnTouchListener {
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
+            if (e1 == null || e2 == null) {
+                return false;
+            }
             try {
-                if (e1!=null && e2!=null) {
-                    try {
-                        float diffY = e2.getY() - e1.getY();
-                        float diffX = e2.getX() - e1.getX();
-                        if (Math.abs(diffX) > Math.abs(diffY)) {
-                            if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                                if (diffX > 0) {
-                                    onSwipeRight();
-                                } else {
-                                    onSwipeLeft();
-                                }
-                            }
-                        } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                            if (diffY > 0) {
-                                onSwipeBottom();
-                            } else {
-                                onSwipeTop();
-                            }
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
                         }
-                    } catch (Exception exception) {
-                        Log.i("Petal", "onFling:" + exception);
+                    }
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom();
+                    } else {
+                        onSwipeTop();
                     }
                 }
-            } catch (Exception e) {
-                Log.v(TAG, "Failed to swipe");
+            } catch (Exception exception) {
+                Log.i("Petal", "onFling:" + exception);
             }
 
-            assert e2 != null;
             return super.onFling(e1, e2, velocityX, velocityY);
         }
     }
