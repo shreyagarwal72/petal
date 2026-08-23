c66bf0850ec21a118d64e49b2767427314708378
Vanshu Agarwal <sanjayvansu1973@gmail.com>
2026-08-23 14:57:05 +0530

    Add files via upload

--- app/src/main/java/com/petal/browser/widget/glance/PetalSearchGlanceWidget.kt
+++ app/src/main/java/com/petal/browser/widget/glance/PetalSearchGlanceWidget.kt
@@ -34,7 +34,6 @@ import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
-import androidx.glance.layout.defaultWeight
 import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
