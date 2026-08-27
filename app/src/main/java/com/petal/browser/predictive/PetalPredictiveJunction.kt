ee49d7df6f9047b3e98b6ca03d00bc9e4cb40c0d
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-08-27 11:54:37 +0530

    Import updated PetalPredictiveJunction.kt

--- app/src/main/java/com/petal/browser/predictive/PetalPredictiveJunction.kt
+++ app/src/main/java/com/petal/browser/predictive/PetalPredictiveJunction.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.graphicsLayer
