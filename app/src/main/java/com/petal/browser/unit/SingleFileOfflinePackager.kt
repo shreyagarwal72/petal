54c5ad292157425b7e1c00a6dfa8488a0d67aefd
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-08-29 00:01:55 +0530

    Refactor: convert initial utility helper modules from Java to Kotlin

--- app/src/main/java/com/petal/browser/unit/SingleFileOfflinePackager.kt
+++ app/src/main/java/com/petal/browser/unit/SingleFileOfflinePackager.kt
@@ -1,19 +1,20 @@
-package com.petal.browser.unit;
+package com.petal.browser.unit
 
 /**
  * SingleFileOfflinePackager generates JavaScript code for bundling entire web pages into single self-contained offline HTML files.
  */
-public class SingleFileOfflinePackager {
+object SingleFileOfflinePackager {
 
     /**
      * Returns JavaScript snippet to serialize full DOM with embedded CSS & images into single string.
      */
-    public static String getSingleFileSerializationJs() {
+    @JvmStatic
+    fun getSingleFileSerializationJs(): String {
         return "(function() {\n" +
                "  try {\n" +
                "    var html = document.documentElement.outerHTML;\n" +
                "    return html;\n" +
                "  } catch(e) { return ''; }\n" +
-               "})();";
+               "})();"
     }
 }
