4ab1448017eb45c53ecdc6df28b4b1660489ad5f
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-09-06 15:20:58 +0530

    Fix build: Add missing BorderStroke import in DisplaySettingsScreen

--- app/src/main/java/com/petal/browser/compose/settings/screens/DisplaySettingsScreen.kt
+++ app/src/main/java/com/petal/browser/compose/settings/screens/DisplaySettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.petal.browser.compose.settings.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
