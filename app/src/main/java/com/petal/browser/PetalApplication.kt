365c3983ea2d34c5aa434d83f422d249700a6779
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-09-07 13:34:02 +0530

    Fix: Capture native process crashes and exit reasons in PetalAppLogger

--- app/src/main/java/com/petal/browser/PetalApplication.kt
+++ app/src/main/java/com/petal/browser/PetalApplication.kt
@@ -73,6 +73,7 @@ class PetalApplication : Application() {
             override fun onActivityStopped(activity: Activity) {
                 startedActivityCount = maxOf(0, startedActivityCount - 1)
                 if (startedActivityCount == 0 && activity.isFinishing) {
+                    com.petal.browser.logger.PetalAppLogger.markCleanExit(this@PetalApplication)
                     val sp = PreferenceManager.getDefaultSharedPreferences(this@PetalApplication)
                     if (sp.getBoolean("sp_clear_quit", false) || sp.getBoolean("sp_clear_on_exit", false)) {
                         BrowserUnit.clearOnExit(this@PetalApplication)
@@ -82,6 +83,7 @@ class PetalApplication : Application() {
             override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
             override fun onActivityDestroyed(activity: Activity) {
                 if (activity.isFinishing) {
+                    com.petal.browser.logger.PetalAppLogger.markCleanExit(this@PetalApplication)
                     val sp = PreferenceManager.getDefaultSharedPreferences(this@PetalApplication)
                     if (sp.getBoolean("sp_clear_quit", false) || sp.getBoolean("sp_clear_on_exit", false)) {
                         BrowserUnit.clearOnExit(this@PetalApplication)
