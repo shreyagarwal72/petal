f27fa10cc9983a1dac3ee3de24ee34452c51197b
Vanshu Agarwal <sanjayvansu1973@gmail.com>
2026-09-18 23:08:51 +0530

    fix(ui): import combinedClickable in PetalBottomNavBar

--- app/src/main/java/com/petal/browser/ui/components/PetalBottomNavBar.kt
+++ app/src/main/java/com/petal/browser/ui/components/PetalBottomNavBar.kt
@@ -42,6 +42,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
